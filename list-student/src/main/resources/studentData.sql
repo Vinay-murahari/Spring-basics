913127aa80882360d581ff62e643770b22a28f94
Vinay-murahari <70923761+Vinay-murahari@users.noreply.github.com>
2020-11-26 11:01:00 +1100

    Add files via upload

---
+++ list-student/src/main/resources/studentData.sql
@@ -0,0 +1,5 @@
+insert into student.student(student_id,student_fname,student_lname,student_adress)
+values(101,"Vinay","Murahari","Boxhill"),
+(102,"Vaishali","Thakoor","Burwood"),
+(103,"Awadhesh","Kumar","Melbourne"),
+(104,"Kiran","Kranthi","Yemmiganur");
